afaacd667b87bc18cd693af399ea084d8d7d578f
Eric Marzouk <ericmarzouk@gmail.com>
2022-09-10 01:09:49 -0700

    radio options fit player type

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import json
+
 import dash
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, ctx
 import requests
 import pandas as pd
 import plotly.express as px
@@ -53,31 +55,112 @@ def get_general_stats(player_name):
 app = Dash(__name__)
 
 app.layout = html.Div([
-    dcc.Dropdown(list(PLAYER_DF.index), value=f"{list(PLAYER_DF.index)[1]}", id="send_player"),
-    dcc.RadioItems(list(FILTER_DICT.keys()), value="Goals", id="filter-input"),
+    dcc.Store(id="player_data_store"),
+    dcc.Dropdown(list(PLAYER_DF.index), value=f"{list(PLAYER_DF.index)[1]}", id="player_list"),
+    dcc.Dropdown(id="team_list", value="All Teams", placeholder="All Teams"),
+    dcc.Store(id="store_player_name"),
+    dcc.RadioItems(list(FILTER_DICT.keys()), id="filter_input"),
     # dcc.Dropdown(list(PLAYER_DF.index), value=f"{list(PLAYER_DF.index)[1]}")
     # dcc.Graph()
-    dcc.Graph(id="player_data"),
-    dcc.Store(id="store-player-stats") #need to convert back to df
+    html.Div(id="sendhere"),
+    dcc.Graph(id="player_data") #need to convert back to df
+    # dcc.Store(id="team_list")
 ])
 
 @app.callback([
-    Output(component_id="player_data", component_property="figure"),
-    Output(component_id="store-player-stats", component_property="data")],
-    [Input(component_id="send_player", component_property='value'),
-    Input(component_id="filter-input", component_property='value')]
+    Output(component_id="team_list", component_property="options"),
+    Output(component_id="team_list", component_property="value"),
+    Output(component_id="store_player_name", component_property="data"),
+    Output(component_id="player_data_store", component_property="data")
+],
+    Input(component_id="player_list", component_property="value")
 )
-def return_player_stats(player_name, filter_input):
+def update_dropdown(player_name):
     general_stats_df, teams_played_for = get_general_stats(player_name)
-    position = PLAYER_DF.loc[player_name]["position.name"]
-    df = general_stats_df.set_index("team.name")
+    temp_list = ["All Teams"]
+    temp_list.extend(teams_played_for)
+    json_general_stats_df = general_stats_df.to_json()
+    return temp_list, "All Teams", player_name, json_general_stats_df
 
-    selected_filter = filter_input
-    if selected_filter in DIGIT_FILTERS:
-        general_stats_df = general_stats_df.groupby('season').sum()
-    fig = px.line(general_stats_df, x=general_stats_df.index, y=f"{FILTER_DICT[selected_filter]}",
-                  hover_name="stat.games")
-    return fig, df.to_dict()
+@app.callback(
+    # Output(component_id="player_data", component_property="figure"),
+    Output(component_id="filter_input", component_property="options"),
+    Input(component_id="team_list", component_property="value"),
+    Input(component_id="store_player_name", component_property="data")
+)
+def update_stat_options(team, player_name):
+    position = PLAYER_DF.loc[player_name]["position.name"]
+    if position == "Goalie":
+        # stat_options = ["stat.shutouts", "stat.ties",
+        #                 "stat.wins", "stat.losses",
+        #                 "stat.goalAgainstAverage",
+        #                 "stat.games", "stat.goalsAgainst"]
+        stat_options = ["GAA", "Shutouts", "Wins", "Losses", "Ties", "Games"]
+    else:
+        stat_options = ["Goals", "Assists", "Points", "Games", "Penalty Minutes", "Plus/Minus"]
+    return stat_options
+
+# @app.callback(
+#     [
+#         Output(component_id="player_data", component_property="figure")],
+#     [
+#         Input(component_id="filter_input", component_property="value"),
+#         Input(component_id="store-player-stats", component_property="value")]
+# )
+# def return_player_graph(selected_filter, general_stats_df):
+#     general_stats_df = pd.DataFrame(general_stats_df)
+#     if selected_filter in DIGIT_FILTERS:
+#         general_stats_df = general_stats_df.groupby('season').sum()
+#     fig = px.line(general_stats_df, x=general_stats_df.index, y=f"{FILTER_DICT[selected_filter]}",
+#                   hover_name="stat.games")
+# @app.callback([
+#     Output(component_id="player_data", component_property="figure"),
+#     Output(component_id="store-player-stats", component_property="data"),
+#     Output(component_id="team_list", component_property="options")],
+#     [Input(component_id="send_player", component_property='value'),
+#      Input(component_id="team_list", component_property="value"),
+#      Input(component_id="filter_input", component_property="value")
+#     ]
+# )
+# def return_player_stats(player_name, team_name, filter_input):
+#     trigger_id = ctx.triggered_id
+#     general_stats_df, teams_played_for = get_general_stats(player_name)
+#     # df = general_stats_df.set_index("team.name")
+#
+#     # if trigger_id == "send_player":
+#     #
+#     # elif trigger_id == "send_team":
+#     #     general_stats_df = pd.DataFrame(player_data)
+#
+#     position = PLAYER_DF.loc[player_name]["position.name"]
+#     selected_filter = filter_input
+#
+#     fig = px.line(general_stats_df, x=general_stats_df.index, y=f"{FILTER_DICT[selected_filter]}")
+#
+#
+#     if trigger_id == "send_player":
+#         if selected_filter in DIGIT_FILTERS:
+#             general_stats_df = general_stats_df.groupby('season').sum()
+#         fig = px.line(general_stats_df, x=general_stats_df.index, y=f"stat.goals")
+#     elif trigger_id == "filter_input":
+#         general_stats_df = general_stats_df.loc[general_stats_df["team.name"] == team_name]
+#         # if selected_filter in DIGIT_FILTERS:
+#         #     general_stats_df = general_stats_df.groupby('season').sum()
+#         fig = px.line(general_stats_df, x="season", y=f"{FILTER_DICT[selected_filter]}")
+#     elif trigger_id == "team_list":
+#         # copy_df = general_stats_df.copy()
+#         # general_stats_df = general_stats_df.set_index('team.name')
+#         # general_stats_df = general_stats_df.loc[team_name.split(" (", 1)[0]]
+#         team_specific_df = general_stats_df.loc[general_stats_df["team.name"]==team_name]
+#         fig = px.line(team_specific_df, x="season", y=f"stat.goals")
+#
+#         if selected_filter in DIGIT_FILTERS:
+#             general_stats_df = general_stats_df.groupby('season').sum()
+#         fig = px.line(general_stats_df, x=general_stats_df.index, y=f"{FILTER_DICT[selected_filter]}")
+#     else:
+#         fig = px.line(general_stats_df.set_index("season"), x=general_stats_df.index, y=f"stat.goals")
+#
+#     return fig, df.to_dict(), teams_played_for
 
 # def
 
